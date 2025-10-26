1c2260796e6e469490828b310048cefb25a28cc0
nico <nico>
2025-10-26 15:43:47 +0800

    南瓜 06:08:294

---
+++ f0.py
@@ -0,0 +1,7 @@
+x = get_pos_x()
+y = get_pos_y()
+quick_print(get_tick_count())
+if(True or False):
+    quick_print(get_tick_count())
+quick_print("x" , "11")
+quick_print(get_tick_count())
--- simute.py
+++ simute.py
@@ -1,3 +1,3 @@
-run_time = simulate("pumpkin_6x6", Unlocks, {Items.Carrot : 10000000000, Items.Power : 1000000}, {}, -1, 0.5)
-do_a_flip()
+#run_time = simulate("pumpkin_6x6", Unlocks, {Items.Carrot : 10000000000, Items.Power : 1000000}, {}, -1, 10000)
+run_time = simulate("test_rank", Unlocks, {Items.Carrot : 10000000000, Items.Power : 0,Items.Pumpkin : 20000000}, {}, -1, 100)
 print(run_time)
